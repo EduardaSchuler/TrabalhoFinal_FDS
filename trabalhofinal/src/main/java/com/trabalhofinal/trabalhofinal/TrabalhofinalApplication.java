041f8061274e0d59c8ad79a3df08d4756d185257
eduardaschuler <dudaschuler2011@gmail.com>
2024-06-12 18:16:33 -0300

    cria projeto java-maven

---
+++ trabalhofinal/src/main/java/com/trabalhofinal/trabalhofinal/TrabalhofinalApplication.java
@@ -0,0 +1,13 @@
+package com.trabalhofinal.trabalhofinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhofinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhofinalApplication.class, args);
+	}
+
+}
---
+++ trabalhofinal/src/test/java/com/trabalhofinal/trabalhofinal/TrabalhofinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabalhofinal.trabalhofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhofinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

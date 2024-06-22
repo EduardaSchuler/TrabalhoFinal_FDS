996a889eb722aada1d4075c8a215e86c865c0bbc
Larissa Oliveira da Silva <108029019+LarissaOliverr@users.noreply.github.com>
2024-06-22 20:21:57 +0000

    implementando IClienteRepository, ClienteService e ClienteRepositoryImpl

---
+++ trabalhofinal/src/main/java/com/trabalhofinal/adapters/persistencia/ClienteRepositoryImpl.java
@@ -0,0 +1,53 @@
+package com.trabalhofinal.adapters.persistencia;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.trabalhofinal.domain.model.Cliente;
+import com.trabalhofinal.domain.repository.IClienteRepository;
+
+@Repository
+public class ClienteRepositoryImpl implements IClienteRepository{
+
+    List<Cliente> clientes;
+    
+     public ClienteRepositoryImpl(){
+        clientes = new LinkedList<Cliente>();
+        clientes.add(new Cliente(2401, "Joana Palmeiras", "joanapalm@gmail.com"));
+        clientes.add(new Cliente(2402, "Vitor Ulisses Machado", "vitorulisses@outlook.com"));
+        clientes.add(new Cliente(2403, "Daniela Borges", "danieborges@hotmail.com"));
+        clientes.add(new Cliente(2404, "Roberto Nascimento Ferreira", "robertonasc@yahoo.com"));
+    }
+
+    @Override
+    public List<Cliente> todos(){
+        return clientes;
+    }
+
+    @Override
+    public Cliente consultaPorCodigo(long codigo){
+        return clientes.stream()
+            .filter(app->app.getCodigo() == codigo)
+            .findFirst()
+            .orElse(null);
+    }
+
+    @Override
+    public boolean cadastrarNovo(Cliente cliente){
+        return clientes.add(cliente);
+    }
+
+    @Override
+    public Cliente editarNome(long codigo, String nomeNovo){
+        consultaPorCodigo(codigo).setNome(nomeNovo);
+        return consultaPorCodigo(codigo);
+    }
+    
+    @Override
+    public Cliente editarEmail(long codigo, String emailNovo){
+        consultaPorCodigo(codigo).setNome(emailNovo);
+        return consultaPorCodigo(codigo);
+    }
+}
--- trabalhofinal/src/main/java/com/trabalhofinal/domain/repository/IClienteRepository.java
+++ trabalhofinal/src/main/java/com/trabalhofinal/domain/repository/IClienteRepository.java
@@ -1,12 +1,14 @@
 package com.trabalhofinal.domain.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import com.trabalhofinal.domain.model.Cliente;
 
 public interface IClienteRepository {
-    List<Cliente> findAll();
-    Optional<Cliente> findById(Long id);
-    Cliente save(Cliente cliente);
+    List<Cliente> todos();
+    Cliente consultaPorCodigo(long codigo);
+    boolean cadastrarNovo(Cliente cliente);
+    Cliente editarNome(long codigo, String nomeNovo);
+    Cliente editarEmail(long codigo, String emailNovo);
+     
 }
